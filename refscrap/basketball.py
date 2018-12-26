51ebd5735f1491d10f9ff436bc3ba180d529391b
Lyndon Shi <shilyndon@gmail.com>
2018-12-25 20:51:15 -0500

    fix getting player page

--- refscrap/basketball.py
+++ refscrap/basketball.py
@@ -5,11 +5,11 @@ import requests
 class BasketballReferenceScraper:
     @property
     def _url(self):
-        return 'https://www.basketball-reference.com/'
+        return 'https://www.basketball-reference.com'
 
     @property
     def _player_index_url(self):
-        return self._url + 'players/'
+        return self._url + '/players'
 
     def scrape(self):
         for letter in 'abcdefghijklmnopqrstuvwxyz':
@@ -18,13 +18,14 @@ class BasketballReferenceScraper:
     def scrape_player_index(self, first_letter):
         if first_letter != 'a' and first_letter != 'x':
             return
-        url = self._player_index_url + first_letter
+        url = self._player_index_url + '/' + first_letter
+
         page_response = requests.get(url, timeout=5)
         if page_response.status_code != 200:
             return
 
         page_content = BeautifulSoup(page_response.content, 'html.parser')
-        table_div = page_content.find('div', {'id', 'all_players'})
+        table_div = page_content.find('div', {'id': 'all_players'})
         player_table = table_div.findChildren('table')[0]
         rows = player_table.findChildren('tr')[1:]
 
@@ -37,4 +38,5 @@ class BasketballReferenceScraper:
             self.scrape_player(self._url + suffix)
 
     def scrape_player(self, url):
-        pass
+        print(url)
+        exit()
