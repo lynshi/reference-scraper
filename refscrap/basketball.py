098dae4c08ab534bf8ea08aa11b3fe2027bb7438
Lyndon Shi <shilyndon@gmail.com>
2018-12-25 20:43:08 -0500

    extract player url

--- refscrap/basketball.py
+++ refscrap/basketball.py
@@ -23,4 +23,18 @@ class BasketballReferenceScraper:
         if page_response.status_code != 200:
             return
 
-        page_content = BeautifulSoup(page_response.content, "html.parser")
+        page_content = BeautifulSoup(page_response.content, 'html.parser')
+        table_div = page_content.find('div', {'id', 'all_players'})
+        player_table = table_div.findChildren('table')[0]
+        rows = player_table.findChildren('tr')[1:]
+
+        for row in rows:
+            strong = row.findChildren('strong')
+            if len(strong) == 0:
+                continue
+
+            suffix = row.findChildren('a')[0]['href']
+            self.scrape_player(self._url + suffix)
+
+    def scrape_player(self, url):
+        pass
