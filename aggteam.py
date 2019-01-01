4be299ae55fea0c2d2ab44f98b1e653b1da514f7
Lyndon Shi <shilyndon@gmail.com>
2019-01-01 17:45:37 -0500

    use actual json

--- aggteam.py
+++ aggteam.py
@@ -3,7 +3,7 @@ from refscrap.aggteam import *
 
 def aggregate_and_upload():
     df = create_aggregated_data_frame('data/basketball/')
-    uploader = Uploader('player_dict_cp.json')
+    uploader = Uploader('player_dict.json')
     get_game_logs(df, uploader)
 
 
